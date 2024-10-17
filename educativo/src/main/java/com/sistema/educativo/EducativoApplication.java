b1da4d6e9f60bce4c52ba05f0b005b039a4820e9
Pokerlan24 <cmoralesm13@miumg.edu.gt>
2024-10-16 20:23:20 -0600

    Primera versión del proyecto

---
+++ educativo/src/main/java/com/sistema/educativo/EducativoApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.educativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducativoApplication.class, args);
+	}
+
+}
---
+++ educativo/src/test/java/com/sistema/educativo/EducativoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.educativo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducativoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
